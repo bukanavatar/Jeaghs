6ecce463c0e0ea5aa38bb018725184b112d9a977
milhamm <m.ilham.mubarak@gmail.com>
2018-08-04 18:16:58 +0700

    Add Modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import {Button, Modal} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.state = {
+            show: false
+        };
+    }
+
+    handleShow() {
+        this.setState({
+            show: true
+        });
+    }
+
+    handleClose() {
+        this.setState({
+            show: false
+        });
+    }
   render() {
     return (
       <div className="App">
@@ -10,9 +32,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="container">
+              {}
+              <p onClick={this.handleShow} style={{cursor: 'pointer'}}>Add Alarm</p>
+              <Modal show={this.state.show} onHide={this.handleClose}>
+                  <Modal.Body>
+
+                  </Modal.Body>
+                  <Modal.Footer>
+                      <Button onClick={this.handleClose}>Cancel</Button>
+                      <Button bsStyle="primary" onClick={this.handleClose}>Save</Button>
+                  </Modal.Footer>
+              </Modal>
+          </div>
       </div>
     );
   }
